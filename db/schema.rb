ecc2e5adfe81fb097e98f6ca49e018a2429d187f
Mike Adeleke <madeleke13@gmail.com>
2014-04-19 08:37:49 -0400

    updated user profile and added bio

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419075309) do
+ActiveRecord::Schema.define(version: 20140419122604) do
 
   create_table "mooches", force: true do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140419075309) do
     t.string   "uid"
     t.string   "name"
     t.string   "username"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
